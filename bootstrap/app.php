b50ca8e0a2ed9396ca115d020956401e309ce146
Annisa <annisafitriana38@gmail.com>
2025-11-06 16:06:57 +0700

    done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
    ->withMiddleware(function (Middleware $middleware) {
     $middleware->alias([
         'admin' => \App\Http\Middleware\CheckAdmin::class,
-    ]);
+    ]); 
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
